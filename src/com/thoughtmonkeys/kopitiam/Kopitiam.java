23f7cafd8be19703ad64fcf9559c4ded4fe6a638
Ruiwen Chua <ruiwen+github@thoughtmonkeys.com>
2013-08-06 02:32:43 +0800

    Commit latest working changes

--- src/com/thoughtmonkeys/kopitiam/Kopitiam.java
+++ src/com/thoughtmonkeys/kopitiam/Kopitiam.java
@@ -7,16 +7,26 @@ import org.json.JSONObject;
 
 import com.thoughtmonkeys.kopitiam.R;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.admob.android.ads.*;
 
 public class Kopitiam extends Activity {
+	
+	// Dialogs
+	private static final int DIALOG_CONDENSED_WHEN_KOSONG = 1;
+	private static final int DIALOG_KOSONG_WHEN_CONDENSED = 2;
+	
 	
 	private HashMap<String, Button> selections;
 	//private HashMap<String, String[]> drinks;
@@ -40,7 +50,6 @@ public class Kopitiam extends Activity {
 			String json = new String(buffer);
 			drinks = new JSONObject(json);
 			
-			Log.d("JSON", drinks.toString());
         }
         catch(Exception e) {
         	// Whoops
@@ -52,51 +61,58 @@ public class Kopitiam extends Activity {
         
         /** Milk **/
         Button milkNone = (Button)findViewById(R.id.milknone);
-        milkNone.setOnClickListener(setMilk);
+        milkNone.setOnClickListener(addCaption);
         
         Button milkCondensed = (Button)findViewById(R.id.milkcondensed);
-        milkCondensed.setOnClickListener(setMilk);        
+        milkCondensed.setOnClickListener(addCaption);        
         
         Button milkEvaporated = (Button)findViewById(R.id.milkevaporated);
-        milkEvaporated.setOnClickListener(setMilk);
+        milkEvaporated.setOnClickListener(addCaption);
         
         /** Strength **/
         Button strengthWeak = (Button)findViewById(R.id.strengthweak);
-        strengthWeak.setOnClickListener(setStrength);
+        strengthWeak.setOnClickListener(addCaption);
         
         Button strengthNormal = (Button)findViewById(R.id.strengthnormal);
-        strengthNormal.setOnClickListener(setStrength);
+        strengthNormal.setOnClickListener(addCaption);
         
         Button strengthStrong = (Button)findViewById(R.id.strengthstrong);
-        strengthStrong.setOnClickListener(setStrength);
+        strengthStrong.setOnClickListener(addCaption);
         
         /** Sweetness **/
         Button sweetnessNone = (Button)findViewById(R.id.sweetnessnone);
-        sweetnessNone.setOnClickListener(setSweetness);
+        sweetnessNone.setOnClickListener(addCaption);
         
         Button sweetnessLess = (Button)findViewById(R.id.sweetnessless);
-        sweetnessLess.setOnClickListener(setSweetness);
+        sweetnessLess.setOnClickListener(addCaption);
         
         Button sweetnessNormal = (Button)findViewById(R.id.sweetnessnormal);
-        sweetnessNormal.setOnClickListener(setSweetness);
+        sweetnessNormal.setOnClickListener(addCaption);
         
         Button sweetnessMore = (Button)findViewById(R.id.sweetnessmore);
-        sweetnessMore.setOnClickListener(setSweetness);
+        sweetnessMore.setOnClickListener(addCaption);
         
         
         /** Ice **/
         Button iceYes = (Button)findViewById(R.id.iceyes);
-        iceYes.setOnClickListener(setIce);
+        iceYes.setOnClickListener(addCaption);
         
         Button iceNo = (Button)findViewById(R.id.iceno);
-        iceNo.setOnClickListener(setIce);
+        iceNo.setOnClickListener(addCaption);
         
         
         /** Set the defaults **/
         selections.put("milk", milkCondensed);
+        milkCondensed.setBackgroundResource(R.drawable.roundbuttonselected);
+        
         selections.put("strength", strengthNormal);
+        strengthNormal.setBackgroundResource(R.drawable.roundbuttonselected);
+        
         selections.put("sweetness", sweetnessNormal);
+        sweetnessNormal.setBackgroundResource(R.drawable.roundbuttonselected);
+        
         selections.put("ice", iceNo);
+        iceNo.setBackgroundResource(R.drawable.roundbuttonselected);
     }
     
     private void setCaption() {
@@ -123,40 +139,62 @@ public class Kopitiam extends Activity {
     	kopiCaption.setText(caption.toString().toUpperCase());    	
     }
     
-    private void addCaption(String key, Button b) {
-    	selections.put(key,b);
+    private OnClickListener addCaption = new OnClickListener() {
+    	public void onClick(View v) {
+    		Button b = (Button)v;
+    		RelativeLayout r = (RelativeLayout)b.getParent();
+    		String tag = r.getTag().toString();
+    		
+    		// Check if we have a CondensedMilk / Kosong situation
+    		if(b.getText().toString().equalsIgnoreCase("condensed") && selections.get("sweetness").getText().toString().equalsIgnoreCase("none")) {
+    			showDialog(DIALOG_CONDENSED_WHEN_KOSONG);
+    			return;
+    		}
+    		else if(b.getText().toString().equalsIgnoreCase("none") && tag.equalsIgnoreCase("sweetness") && selections.get("milk").getText().toString().equalsIgnoreCase("condensed")) {
+    			showDialog(DIALOG_KOSONG_WHEN_CONDENSED);
+    			return;
+    		}
+    		
+    		Button ob = selections.get(r.getTag().toString());
+    		ob.setBackgroundResource(R.drawable.roundbutton);
+    		b.setBackgroundResource(R.drawable.roundbuttonselected);
+    		
+    		selections.put(r.getTag().toString(), b);
+    		
+    		setCaption();
+    	}    	
+    };
+    
+    
+    @Override
+    protected Dialog onCreateDialog(int id) {
+    	switch(id) {
+    	case DIALOG_CONDENSED_WHEN_KOSONG: 
+    		return new AlertDialog.Builder(Kopitiam.this)
+    					.setTitle("Unable to select condensed milk.")
+    					.setMessage("You have indicated that you'd like your coffee unsweetened. Choosing Condensed milk now will make it sweet. Please make another milk selection.")
+    					.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+    				           public void onClick(DialogInterface dialog, int id) {
+    				                // End
+    				        	   Kopitiam.this.removeDialog(id);
+    				           }
+    				       })
+    				       .create();    		
     	
-    	setCaption();
+    	case DIALOG_KOSONG_WHEN_CONDENSED: 
+    		return new AlertDialog.Builder(Kopitiam.this)
+    					.setTitle("Unable to select unsweetened coffee.")
+    					.setMessage("You have indicated that you'd like Condensed milk in your coffee, which makes it sweet. Please choose another level of sweetness, like Less.")
+    					.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+    				           public void onClick(DialogInterface dialog, int id) {
+    				                // End
+    				        	   Kopitiam.this.removeDialog(id);
+    				           }
+    				       })
+    				       .create();
+    		
+    	} 
+    	
+    	return null;
     }
-    
-    private OnClickListener setMilk = new OnClickListener() {    	
-        public void onClick(View v) {
-        	Button b = (Button)v;
-   		 	//Toast.makeText(Kopitiam.this, b.getText(),
-            //     Toast.LENGTH_SHORT).show();  
-   		 	
-   		 	addCaption("milk", b);
-        }
-    };
-    
-    private OnClickListener setStrength = new OnClickListener() {
-    	public void onClick(View v) {
-    		Button b = (Button)v;
-   		 	addCaption("strength", b);
-    	}    	
-    };
-    
-    private OnClickListener setSweetness = new OnClickListener() {
-    	public void onClick(View v) {
-    		Button b = (Button)v;
-   		 	addCaption("sweetness", b);
-    	}    	
-    };
-    
-    private OnClickListener setIce = new OnClickListener() {
-    	public void onClick(View v) {
-    		Button b = (Button)v;
-   		 	addCaption("ice", b);
-    	}    	
-    };    
 }
